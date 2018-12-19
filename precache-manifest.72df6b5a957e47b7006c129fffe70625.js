cf5472e2733f23b6b37605f11a894b222fc8957c
gidraff <kamandegid@gmail.com>
2018-12-19 13:03:18 +0300

    Updates

---
+++ precache-manifest.72df6b5a957e47b7006c129fffe70625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1f0956d513ccbfed8b3",
+    "url": "/static/js/main.b1f0956d.chunk.js"
+  },
+  {
+    "revision": "50ccbd609e4823a5253f",
+    "url": "/static/js/1.50ccbd60.chunk.js"
+  },
+  {
+    "revision": "b1f0956d513ccbfed8b3",
+    "url": "/static/css/main.a604a224.chunk.css"
+  },
+  {
+    "revision": "1b4f7aea5b76802d73bd8ab27ee91162",
+    "url": "/index.html"
+  }
+];
